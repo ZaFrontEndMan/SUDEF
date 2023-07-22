8dae753a9a78bd1b1ca6ee2dfdd395853a386829
ZaFrontEndMan <128174502+ZaFrontEndMan@users.noreply.github.com>
2023-07-22 06:17:54 +0300

    Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,12 +5,27 @@ export default function Product({product , setProduct }) {
 
     return (
     <>
-    <div  className=" col-lg-4 col-md-6 col-sm-12 p-5 hover" onClick={() => setProduct(product)}  >
-                      
-                            <img src={product.image}  data-bs-toggle="modal" data-bs-target="#staticBackdrop"  style={{cursor: 'pointer'}} className="w-100" alt=""  />
-                            <h2 className="h6  py-2">Name  :<span className="text-muted">  {product.title}</span> </h2>
-                            <h2 className="h6  ">    Price :<span className="text-success">{product.price}</span></h2>
-                            </div>
+    <div
+      style={{ display:"flex", flexDirection:"column", justifyContent:"space-between"}} 
+    className=" col-lg-4 col-md-6 col-sm-12 p-5 hover" 
+    onClick={() => setProduct(product)}  >
+
+     <div
+     style={{cursor: 'pointer', height:"300px", objectFit:"contain", display:"flex", justifyContent:"center"}}
+     >                 
+   <img src={product.image}  data-bs-toggle="modal" style={{objectFit:"contain", height:"100%"}} data-bs-target="#staticBackdrop"   className="w-100" alt=""  />
+   </div>
+
+   <div>
+   <div class="row">
+  <div class=" text-truncate">
+  <h2  className="h6  py-2">Name  :<span className="text-muted">  {product.title}</span> </h2>
+  </div>
+</div>
+   <h2 className="h6  ">    Price :<span className="text-success">{product.price}</span></h2>
+   </div>
+   
+   </div>
     </>
   )
 }
